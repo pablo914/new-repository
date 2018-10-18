381683e0a19cf3248644d7b9b8e932de190c77e1
pablo914 <43987189+pablo914@users.noreply.github.com>
2018-10-18 17:18:29 +0100

    Add files via upload

---
+++ if example.py
@@ -0,0 +1,11 @@
+'''a = 13
+b = 14
+if a + 1 <= b:
+    print ("correct")'''
+#myage = input("How old are you") 
+#print("Hi there, you are"=myage="old")
+
+number1 = input("Enter	first	number")
+number2	= input("Enter	second	number")
+result	= number1 + number2
+print(“The result is ”+result)
